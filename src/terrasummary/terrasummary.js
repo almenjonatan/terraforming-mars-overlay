57fc65bac70dd3da88cf0956531c80dfa2beeb7b
Cameron McCallie <cam@cameo.com>
2020-10-06 23:37:49 -0700

    dark mode, more compact

--- src/terrasummary/terrasummary.js
+++ src/terrasummary/terrasummary.js
@@ -5,261 +5,270 @@ import {Seq} from "immutable";
 
 const { Map, fromJS } = require('immutable');
 
-//   const mockData = {
-//    "1":{
-//       "FinalScore":0,
-//       "TerraformingRating":21,
-//       "ResourceData":{
-//          "MegaCredit":{
-//             "Quantity":39,
-//             "Production":0
-//          },
-//          "Steel":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Titanium":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Plant":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Energy":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Heat":{
-//             "Quantity":0,
-//             "Production":3
-//          }
-//       },
-//       "CardsInHand":[
-//          "Colonizer Training Camp",
-//          "Zeppelins",
-//          "Ants",
-//          "Martian Rails",
-//          "Asteroid Mining Consortium",
-//          "Shuttles",
-//          "Imported Nitrogen",
-//          "Trans-Neptune Probe",
-//          "Nitrite Reducing Bacteria",
-//          "Energy Tapping",
-//          "Capital",
-//          "Mangrove",
-//          "Urbanized Area",
-//          "Ice cap Melting",
-//          "Industrial Microbes",
-//          "Building Industries"
-//       ],
-//       "CardsPlayed":[
-//          "Subterranean Reservoir"
-//       ],
-//       "PlayerId":1,
-//       "IsLocalPlayer":true,
-//       "Color":"Green",
-//       "Score":{
-//          "FinalScore":28,
-//          "TerraFormingRating":21,
-//          "VictoryPoints":0,
-//          "GreeneryScore":0,
-//          "CityScore":0,
-//          "MilestoneScore":[
-//             {
-//                "Name":"Planner",
-//                "Score":5
-//             }
-//          ],
-//          "AwardScore":[
-//             {
-//                "Name":"Landlord",
-//                "Score":2
-//             },
-//             {
-//                "Name":"Banker",
-//                "Score":0
-//             },
-//             {
-//                "Name":"Scientist",
-//                "Score":0
-//             },
-//             {
-//                "Name":"Thermalist",
-//                "Score":0
-//             },
-//             {
-//                "Name":"Miner",
-//                "Score":0
-//             }
-//          ]
-//       }
-//    },
-//    "2":{
-//       "FinalScore":0,
-//       "TerraformingRating":26,
-//       "ResourceData":{
-//          "MegaCredit":{
-//             "Quantity":9,
-//             "Production":0
-//          },
-//          "Steel":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Titanium":{
-//             "Quantity":2,
-//             "Production":0
-//          },
-//          "Plant":{
-//             "Quantity":1,
-//             "Production":1
-//          },
-//          "Energy":{
-//             "Quantity":0,
-//             "Production":1
-//          },
-//          "Heat":{
-//             "Quantity":4,
-//             "Production":2
-//          }
-//       },
-//       "CardsInHand":[
-//          "Nitrophilic Moss",
-//          "Fish",
-//          "Physics Complex"
-//       ],
-//       "CardsPlayed":[
-//          "Technology Demonstration",
-//          "Dust Seals",
-//          "Geothermal Power",
-//          "Development Center",
-//          "Deimos Down",
-//          "Tardigrades",
-//          "Magnetic Field Dome",
-//          "Power Plant"
-//       ],
-//       "PlayerId":2,
-//       "IsLocalPlayer":false,
-//       "Color":"Grey",
-//       "Score":{
-//          "FinalScore":33,
-//          "TerraFormingRating":26,
-//          "VictoryPoints":1,
-//          "GreeneryScore":0,
-//          "CityScore":0,
-//          "MilestoneScore":[
-//
-//          ],
-//          "AwardScore":[
-//             {
-//                "Name":"Landlord",
-//                "Score":2
-//             },
-//             {
-//                "Name":"Banker",
-//                "Score":0
-//             },
-//             {
-//                "Name":"Scientist",
-//                "Score":2
-//             },
-//             {
-//                "Name":"Thermalist",
-//                "Score":2
-//             },
-//             {
-//                "Name":"Miner",
-//                "Score":0
-//             }
-//          ]
-//       }
-//    },
-//    "3":{
-//       "FinalScore":0,
-//       "TerraformingRating":21,
-//       "ResourceData":{
-//          "MegaCredit":{
-//             "Quantity":10,
-//             "Production":1
-//          },
-//          "Steel":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Titanium":{
-//             "Quantity":0,
-//             "Production":0
-//          },
-//          "Plant":{
-//             "Quantity":2,
-//             "Production":1
-//          },
-//          "Energy":{
-//             "Quantity":0,
-//             "Production":3
-//          },
-//          "Heat":{
-//             "Quantity":0,
-//             "Production":0
-//          }
-//       },
-//       "CardsInHand":[
-//          "Commercial District",
-//          "Aerobraked Ammonia Asteroid"
-//       ],
-//       "CardsPlayed":[
-//          "Invention Contest",
-//          "Lagrange Observatory",
-//          "Search For Life",
-//          "Archaebacteria",
-//          "Earth Office",
-//          "Regolith Eaters",
-//          "Tectonic Stress Power"
-//       ],
-//       "PlayerId":3,
-//       "IsLocalPlayer":false,
-//       "Color":"Yellow",
-//       "Score":{
-//          "FinalScore":40,
-//          "TerraFormingRating":21,
-//          "VictoryPoints":2,
-//          "GreeneryScore":1,
-//          "CityScore":1,
-//          "MilestoneScore":[
-//
-//          ],
-//          "AwardScore":[
-//             {
-//                "Name":"Landlord",
-//                "Score":5
-//             },
-//             {
-//                "Name":"Banker",
-//                "Score":0
-//             },
-//             {
-//                "Name":"Scientist",
-//                "Score":5
-//             },
-//             {
-//                "Name":"Thermalist",
-//                "Score":5
-//             },
-//             {
-//                "Name":"Miner",
-//                "Score":0
-//             }
-//          ]
-//       }
-//    }
-// };
+  const mockData = {
+   "1":{
+      "FinalScore":0,
+      "TerraformingRating":21,
+      "ResourceData":{
+         "MegaCredit":{
+            "Quantity":39,
+            "Production":0
+         },
+         "Steel":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Titanium":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Plant":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Energy":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Heat":{
+            "Quantity":0,
+            "Production":3
+         }
+      },
+      "CardsInHand":[
+         "Colonizer Training Camp",
+         "Zeppelins",
+         "Ants",
+         "Martian Rails",
+         "Asteroid Mining Consortium",
+         "Shuttles",
+         "Imported Nitrogen",
+         "Trans-Neptune Probe",
+         "Nitrite Reducing Bacteria",
+         "Energy Tapping",
+         "Capital",
+         "Mangrove",
+         "Urbanized Area",
+         "Ice cap Melting",
+         "Industrial Microbes",
+         "Building Industries"
+      ],
+      "CardsPlayed":[
+         "Subterranean Reservoir"
+      ],
+      "PlayerId":1,
+      "IsLocalPlayer":true,
+      "Color":"Green",
+      "Score":{
+         "FinalScore":43,
+         "TerraFormingRating":21,
+         "VictoryPoints":0,
+         "GreeneryScore":0,
+         "CityScore":15,
+         "MilestoneScore":[
+            {
+               "Name":"Planner",
+               "Score":5
+            }
+         ],
+         "AwardScore":[
+            {
+               "Name":"Landlord",
+               "Score":2
+            },
+            {
+               "Name":"Banker",
+               "Score":0
+            },
+            {
+               "Name":"Scientist",
+               "Score":0
+            },
+            {
+               "Name":"Thermalist",
+               "Score":0
+            },
+            {
+               "Name":"Miner",
+               "Score":0
+            }
+         ]
+      }
+   },
+   "2":{
+      "FinalScore":0,
+      "TerraformingRating":26,
+      "ResourceData":{
+         "MegaCredit":{
+            "Quantity":9,
+            "Production":0
+         },
+         "Steel":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Titanium":{
+            "Quantity":2,
+            "Production":0
+         },
+         "Plant":{
+            "Quantity":1,
+            "Production":1
+         },
+         "Energy":{
+            "Quantity":0,
+            "Production":1
+         },
+         "Heat":{
+            "Quantity":4,
+            "Production":2
+         }
+      },
+      "CardsInHand":[
+         "Nitrophilic Moss",
+         "Fish",
+         "Physics Complex"
+      ],
+      "CardsPlayed":[
+         "Technology Demonstration",
+         "Dust Seals",
+         "Geothermal Power",
+         "Development Center",
+         "Deimos Down",
+         "Tardigrades",
+         "Magnetic Field Dome",
+         "Power Plant"
+      ],
+      "PlayerId":2,
+      "IsLocalPlayer":false,
+      "Color":"Grey",
+      "Score":{
+         "FinalScore":43,
+         "TerraFormingRating":26,
+         "VictoryPoints":1,
+         "GreeneryScore":10,
+         "CityScore":0,
+         "MilestoneScore":[
+
+         ],
+         "AwardScore":[
+            {
+               "Name":"Landlord",
+               "Score":2
+            },
+            {
+               "Name":"Banker",
+               "Score":0
+            },
+            {
+               "Name":"Scientist",
+               "Score":2
+            },
+            {
+               "Name":"Thermalist",
+               "Score":2
+            },
+            {
+               "Name":"Miner",
+               "Score":0
+            }
+         ]
+      }
+   },
+   "3":{
+      "FinalScore":0,
+      "TerraformingRating":21,
+      "ResourceData":{
+         "MegaCredit":{
+            "Quantity":10,
+            "Production":1
+         },
+         "Steel":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Titanium":{
+            "Quantity":0,
+            "Production":0
+         },
+         "Plant":{
+            "Quantity":2,
+            "Production":1
+         },
+         "Energy":{
+            "Quantity":0,
+            "Production":3
+         },
+         "Heat":{
+            "Quantity":0,
+            "Production":0
+         }
+      },
+      "CardsInHand":[
+         "Commercial District",
+         "Aerobraked Ammonia Asteroid"
+      ],
+      "CardsPlayed":[
+         "Invention Contest",
+         "Lagrange Observatory",
+         "Search For Life",
+         "Archaebacteria",
+         "Earth Office",
+         "Regolith Eaters",
+         "Tectonic Stress Power"
+      ],
+      "PlayerId":3,
+      "IsLocalPlayer":false,
+      "Color":"Yellow",
+      "Score":{
+         "FinalScore":40,
+         "TerraFormingRating":21,
+         "VictoryPoints":2,
+         "GreeneryScore":1,
+         "CityScore":1,
+         "MilestoneScore":[
+
+         ],
+         "AwardScore":[
+            {
+               "Name":"Landlord",
+               "Score":5
+            },
+            {
+               "Name":"Banker",
+               "Score":0
+            },
+            {
+               "Name":"Scientist",
+               "Score":5
+            },
+            {
+               "Name":"Thermalist",
+               "Score":5
+            },
+            {
+               "Name":"Miner",
+               "Score":0
+            }
+         ]
+      }
+   }
+};
+
+const SMALL_COL_WIDTH = "32px";
+const CELL_PADDING = 3;
+const COL_STYLE = {
+  fontSize: 20,
+  fontWeight: 600,
+  paddingLeft: CELL_PADDING,
+  paddingRight: CELL_PADDING
+};
 
 const customStyles = {
   headCells: {
     style: {
-      paddingLeft: '5px',
-      paddingRight: '5px',
+      paddingLeft: CELL_PADDING,
+      paddingRight: CELL_PADDING,
     }
   }
 }
@@ -361,19 +370,13 @@ class TerraSummary extends React.Component {
 
             s = s.set("MileStones", value
                 .getIn(["Score", "MilestoneScore"])
-                .filter(value => {
-                    return value.get("Score") > 0
-                })
-                .map(value => value.get("Name") + " (" + value.get("Score") + ")")
-                .join(", "))
+                .map(value => value.get("Score"))
+                .reduce((acc, val) => acc + val) || 0);
 
             s = s.set("Awards", value
                 .getIn(["Score", "AwardScore"])
-                .filter(value => {
-                    return value.get("Score") > 0
-                })
-                .map(value => value.get("Name") + " (" + value.get("Score") + ")")
-                .join(", "))
+                .map(value => value.get("Score"))
+                .reduce((acc, val) => acc + val) || 0);
 
 
             // subtract awards score from total if checked
@@ -391,115 +394,93 @@ class TerraSummary extends React.Component {
 
         console.log(scores.toJS())
 
-        const SMALL_WIDTH = "45px";
-
         let columns = [
             {
                 name: "Player",
                 selector: "Color",
-                width: "55px",
+                width: "50px",
                 style: {
-                  paddingLeft: 5,
-                  paddingRight: 5
+                  paddingLeft: CELL_PADDING,
+                  paddingRight: CELL_PADDING
                 },
                 conditionalCellStyles: [
                   {
                     when: row => row.Color === 'Purple',
                     style: {
                       backgroundColor: '#ad4fe8',
+                      fontWeight: 600
                     }
                   },
                   {
                     when: row => row.Color === 'Green',
                     style: {
                       backgroundColor: '#48cf4b',
+                      fontWeight: 600
                     }
                   },
                   {
                     when: row => row.Color === 'Yellow',
                     style: {
-                      backgroundColor: '#e0da2b'
+                      backgroundColor: '#e0da2b',
+                      fontWeight: 600
                     }
                   },
                   {
                     when: row => row.Color === 'Red',
                     style: {
-                      backgroundColor: '#e32441'
+                      backgroundColor: '#e32441',
+                      fontWeight: 600
                     }
                   },
                   {
                     when: row => row.Color === 'Blue',
                     style: {
-                      backgroundColor: '#2470e3'
+                      backgroundColor: '#2470e3',
+                      fontWeight: 600
                     }
                   },
                 ]
             },
             {
-                name: "Sum",
-                selector: "FinalScore",
-                width: SMALL_WIDTH,
-                style: {
-                  fontSize: 20,
-                  paddingLeft: 5,
-                  paddingRight: 5
-                }
+              name: "Sum",
+              selector: "FinalScore",
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             },
             {
-                name: "Cty",
-                selector: "CityScore",
-                width: SMALL_WIDTH,
-                style: {
-                  fontSize: 20,
-                  paddingLeft: 5,
-                  paddingRight: 5
-                }
+              name: "Cty",
+              selector: "CityScore",
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             },
             {
-                name: "Grn",
-                selector: "GreeneryScore",
-                width: SMALL_WIDTH,
-                style: {
-                  fontSize: 20,
-                  paddingLeft: 5,
-                  paddingRight: 5
-                }
+              name: "Grn",
+              selector: "GreeneryScore",
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             },
             {
-                name: "VP",
-                selector: "VictoryPoints",
-                width: SMALL_WIDTH,
-                style: {
-                  fontSize: 20,
-                  paddingLeft: 5,
-                  paddingRight: 5
-                }
+              name: "VP",
+              selector: "VictoryPoints",
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             },
             {
-                name: "MS",
-                selector: "MileStones",
-                width: "120px",
-                wrap: true,
-                style: {
-                  paddingLeft: 5,
-                  paddingRight: 5
-                }
+              name: "MS",
+              selector: "MileStones",
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             },
             {
-              name: "Awards",
+              name: "Aw",
               selector: "Awards",
-              width: "120px",
-              compact: true,
-              wrap: true,
-              style: {
-                paddingLeft: 5,
-                paddingRight: 5
-              }
+              width: SMALL_COL_WIDTH,
+              style: COL_STYLE
             }
         ];
 
         if (!this.state.countAwards) {
-          columns = columns.filter((column) => column.name !== 'Awards');
+          columns = columns.filter((column) => column.name !== 'Aw');
         }
 
         if (this.state.compact) {
@@ -511,13 +492,18 @@ class TerraSummary extends React.Component {
             columns={columns}
             data={scores.toJS()}
             dense={true}
+            theme='dark'
             noHeader
             subHeader
             subHeaderComponent={
               <div style={{ position: 'absolute', left: 0}}>
-                Count Awards?
-                <input style={{ marginRight: 10 }} type="checkbox" checked={Boolean(this.state.countAwards)} onChange={this.handleCountAwards}/>
-                Compact?
+                <label style={{ color: 'white' }}>Awards?</label>
+                <input style={{
+                  marginRight: 10,
+                  color: 'white'
+                }} type="checkbox" checked={Boolean(this.state.countAwards)} onChange={this.handleCountAwards}/>
+                <br/>
+                <label style={{ color: 'white' }}>Compact?</label>
                 <input type="checkbox" checked={Boolean(this.state.compact)} onChange={this.handleCompact}/>
               </div>
             }
