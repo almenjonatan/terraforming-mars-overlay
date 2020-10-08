a05fac2a14a33133d225d756bd8dd34f3a47b6ab
Jonatan Almen <almen.jonatan@gmail.com>
2020-10-08 02:11:53 +0200

    add static files

---
+++ backend/static/precache-manifest.efabef78f6268c6a00af36767f8d96ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e77315bc00990f7af1b6d325f5b41c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2098b21b067ef62a7d13",
+    "url": "/static/css/main.9a52a38c.chunk.css"
+  },
+  {
+    "revision": "3bf12ddeb1e3956a9b47",
+    "url": "/static/js/2.51eb61a3.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.51eb61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2098b21b067ef62a7d13",
+    "url": "/static/js/main.c1201ce1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
